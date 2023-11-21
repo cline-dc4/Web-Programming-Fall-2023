ef11af78d95b25d997d40f9e8ad5e530228ffce4
DC Cline <cline.dc4@gmail.com>
2023-11-20 20:20:41 -0800

    project done?

--- Homework/Project1/src/GameDisplay.tsx
+++ Homework/Project1/src/GameDisplay.tsx
@@ -48,6 +48,14 @@ let editIsMatched = [false, false, false, false, false,
     false, false, false, false, false,
     false, false, false, false, false,
     false, false, false, false, false];
+// a number that is used for card logic
+let firstSelectedCardNum = -1;
+// a number that is used for card logic
+let firstSelectedId = -1;
+// a number that is used for card logic
+let secondSelectedCardNum = -1;
+// a number that is used for card logic
+let secondSelectedId = -1;
 
 // the function that holds everything else and is called by App.tsx
 function CreateBoard()
@@ -61,12 +69,10 @@ function CreateBoard()
     // An array of booleans that say whether a given card number is matched
     // or not.
     const [isMatched, setIsMatched] = useState(editIsMatched);    
+    // a usestate boolean that disables the onClick while it is timed out.
+    const [isTimedOut, setIsTimedOut] = useState(false);
     // An array of values between 0 and 10 that determine the image.
     const [imageNumbers, setImageNumbers] = useState<Array<Number>>(editImageNumbers);    
-    // stores the key of the first card that was selected.
-    const [firstSelected, setFirstSelected] = useState(null);
-    // stores the key of the second card that was selected.
-    const [secondSelected, setSecondSelected] = useState(null);
 
     // This function just displays the information at the top
     // of the webpage like who's turn it is or how many points they have.
@@ -79,6 +85,17 @@ function CreateBoard()
                 <p>Player 1: {playerOnePoints} points</p>
                 <p>Player 2: {playerTwoPoints} points </p>
             </div>
+        )
+    }
+
+    // This function creates a button that prints out the randomized array
+    // to the console.
+    function DebugButton()
+    {
+        return(
+            <>
+                <button onClick={() => console.log(randomArray)}>Debug</button>
+            </>
         )
     }
 
@@ -106,49 +123,121 @@ function CreateBoard()
             {array.map((subitem:Number) => 
             (<td><img 
                 class="card"
-                id={subitem+1}
-                num={i}
-                src={imageArray[imageNumbers[i]]}
-                disabled={!isMatched[i++]}
-                onClick={(event) => cardPress(subitem+1, event)}>
+                cardNum={subitem+1}
+                id={i}
+                src={imageArray[imageNumbers[i++]]}
+                onClick={(event) => cardPress(event)}>
             </img></td>))}
             </tr>
     }
 
     // This function handles logic when a card is pressed.
-    function cardPress(key:Number, event)
+    function cardPress(event)
     {   
-        if(secondSelected == null)
+        // disable cardPress if the card has been matched or if it is timed out
+        if(!isTimedOut && !isMatched[event.target.getAttribute('id')])
         {
-            let num = Number(event.target.getAttribute('num'))
-            editImageNumbers[Number(num)]=key;
-            setImageNumbers(editImageNumbers)
-            // if none selected, setFirstSelected and allow user
-            // to select the second card
-            if(firstSelected == null)
-                setFirstSelected(event);
-            // if second card is selected, check for match
-            else
+            // if the second card hasn't been selected and it is not the same as the first
+            // selected card, continue.
+            if(secondSelectedId == -1 && event.target.getAttribute('id') != firstSelectedId)
             {
-                setSecondSelected(event);
-                if(firstSelected.target.getAttribute('id') == secondSelected.target.getAttribute('id'))
+                editImageNumbers[event.target.getAttribute('id')] = 
+                    event.target.getAttribute('cardNum');
+                setImageNumbers(editImageNumbers)
+                // if none selected, setFirstSelected and allow user
+                // to select the second card
+                if(firstSelectedId == -1)
                 {
-                    // change turn after 3 seconds and hide cards
-                    setTimeout(() => {
-                    setPlayerTurn(((playerTurn+1)%2))
-                    }, 3000);
+                    firstSelectedId = (Number(event.target.getAttribute('id')));
+                    firstSelectedCardNum = (Number(event.target.getAttribute('cardNum')));
+                }
+                // if second card is selected, check for match and fill in secondSelected
+                else
+                {
+                    secondSelectedId = (Number(event.target.getAttribute('id')));
+                    secondSelectedCardNum = (Number(event.target.getAttribute('cardNum')));
+                    // if match:
+                    if(firstSelectedCardNum == secondSelectedCardNum)
+                    {
+                        // change states of the cards that have matches
+                        editIsMatched[firstSelectedId] = true;
+                        editIsMatched[secondSelectedId] = true;
+                        setIsMatched(editIsMatched);
+                        // award point
+                        if(playerTurn == 0)
+                            setPlayerOnePoints(playerOnePoints + 1)
+                        else
+                            setPlayerTwoPoints(playerTwoPoints + 1)
+                        
+                        // check if everything is matched
+                        setIsTimedOut(true)
+                            setTimeout(() => {
+                            let end = true;
+                            for(let i = 0; i < isMatched.length; i++)
+                            {
+                                if(isMatched[i] == false)
+                                    end = false;
+                            }
+                            // if everything is matched, then end game
+                            if(end)
+                                gameEnd();
+                            setIsTimedOut(false);
+                        }, 100)
+                        // advance turn
+                        setPlayerTurn(((playerTurn+1)%2))
+                        // reset default values
+                        firstSelectedCardNum = (-1);
+                        firstSelectedId = (-1);
+                        secondSelectedCardNum = (-1);
+                        secondSelectedId = (-1);
+                    }
+                    // if not a match:
+                    else
+                    {
+                        // pause for three seconds
+                        setIsTimedOut(true)
+                        setTimeout(() => {
+                            // flip cards back over
+                            editImageNumbers[firstSelectedId] = 0;
+                            editImageNumbers[secondSelectedId] = 0;
+                            setImageNumbers(editImageNumbers);
+                            // advance turn
+                            setPlayerTurn(((playerTurn+1)%2))
+                            // reset default values
+                            firstSelectedCardNum = (-1);
+                            firstSelectedId = (-1);
+                            secondSelectedCardNum = (-1);
+                            secondSelectedId = (-1);
+                            setIsTimedOut(false)
+                        }, 3000);
+                    }
                 }
             }
         }
+    }
 
-
-
+    // handles all the logic for when the game is ended.
+    function gameEnd()
+    {
+        if(playerOnePoints > playerTwoPoints)
+            alert("Player one won! The score is:\n" +
+                "Player 1: " + playerOnePoints + "\n" +
+                "Player 2: " + playerTwoPoints)
+        else if(playerOnePoints < playerTwoPoints)
+            alert("Player two won! The score is:\n" +
+                "Player 1: " + playerOnePoints + "\n" +
+                "Player 2: " + playerTwoPoints)
+        else if(playerOnePoints == playerTwoPoints)
+            alert("Tie! The score is:\n" +
+                "Player 1: " + playerOnePoints + "\n" +
+                "Player 2: " + playerTwoPoints)
     }
 
     return (
         <>
             <GameInfo/>
             <CreateCards/>
+            <DebugButton/>
         </>
     )
 }
@@ -157,7 +246,6 @@ function CreateBoard()
 // randomly assigned to indices.
 function randomizeArray()
 {
-    console.log("Randomize");
     // index used in the randomArray and for the while loop.
     let i = 0;
     // a variable used to store the random number to be added.
@@ -370,7 +458,6 @@ function randomizeArray()
         }
         
     }
-    console.log(returnArray)
     return(returnArray)
 }
 export {CreateBoard}
