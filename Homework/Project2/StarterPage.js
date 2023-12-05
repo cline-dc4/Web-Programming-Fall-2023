2377f15bb4f01ca7fef214a927f7558917444f96
DC Cline <cline.dc4@gmail.com>
2023-12-05 13:16:10 -0800

    significant progress on project 2

--- Homework/Project2/StarterPage.js
+++ Homework/Project2/StarterPage.js
@@ -2,7 +2,7 @@ app.get("/myplan", (req, res) =>
 	{
 		let htmlString = "<html><head><title>CS major</title></head><body>";
 		htmlString += "<h3> Welcome to Corban!</h3>";
-	htmlString += "<form action = 'http://localhost:8888/csmajor' method = 'GET'>";
+	htmlString += "<form action = 'http://localhost:8888/mathmajor' method = 'GET'>";
 		htmlString += "<label> When do you plan to start? </label><input type='number' name='year'></br>";
 		htmlString += "<label> Pick a major: </label><select name='major'>";
         htmlString += "<option value='cs'>Computer Science</option>";
@@ -18,4 +18,11 @@ app.get("/myplan", (req, res) =>
 		console.log("CS" + " year " + year);
 		res.send(readFile(year, "Courses.txt"));
 		
+	});
+
+	app.get("/mathmajor", (req, res) => 
+	{
+		let year = req.query.year;//url.parse(req.url).pathname.split("_")[1];
+		console.log("Math" + " year " + year);
+		res.send(start(year));
 	});
--- Homework/Project2/mainPage.js
+++ Homework/Project2/mainPage.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const url = require('url');
 
-function start(route, handle) 
+function start(year) 
 {
     //start the server
     app.listen(8000, () => 
@@ -13,24 +12,48 @@ function start(route, handle)
 
     app.get("/mathmajor*", (request,response) =>
     {
-        response.send("<h1>Math Major Schedule Creation</h1>"+ createTable());
+        response.send("<h1>Math Major Schedule Creation</h1>"+ createTable(readFile("./courseCatalog.txt")));
     });
 
-    app.get("/generatePlan", (request,response) =>
+    // holds the logic for generating the plan
+    app.get("/generatePlan*", (request,response) =>
     {
         console.log("Method accessed");
-        response.send("<p>hello</p>");
+        // a 2d array that will hold the original data without the classes
+        // that have been selected.
+        let removedClassesArray = [];
+        // this array holds the classes and the semesters they should
+        // be taken in.
+        let semesterPlan = [];
+        // the string that will contain the output schedule.
+        let htmlString = "";
+        // import data from the txt file.
+        let dataArray = readFile("./courseCatalog.txt");
+        console.log(dataArray);
+        let counter = 0;
+        // cut out classes that aren't being used.
+        for (let i = 0; i < dataArray.length; i++)
+        {
+            if (request.query[i] != "default")
+                removedClassesArray[i-counter] = dataArray[i];
+            else
+                counter++;
+        }
+
+        // the absolutly massive for loop that fills out the semester plan array
+        for(let i = 0; i < removedClassesArray; i++)
+        {
+            
+        }
+
+        // display information on the page.
+        response.send("<h3>4 Year Plan:</h3>" + htmlString);
     });
 
+    // reads in the file and returns a 2d array formated for a table.
     function readFile(fileString)
     {
         const data = fs.readFileSync(fileString, {encoding: 'utf8'});
-        return(data);
-    }
-
-    function createTable()
-    {
-        let data = readFile("./courseCatalog.txt");
         // create an array with each element being all the data for a specific class.
         let dataArray = data.split("\n");
         let tableArray = [];
@@ -40,17 +63,20 @@ function start(route, handle)
             tableArray[i] = dataArray[i].split("\t");
         }
 
+        return(tableArray);
+    }
+
+    function createTable(tableArray)
+    {
+        
         // creating the table: 
         // initlaize and create row for headings
         // and create a form for the submit and radio buttons
         let table = "<form action='/generatePlan' method='GET'>" + 
         "<table border='2'>" +
         "<tr>" +
-            "<th>Earliest Year</th>" +
-            "<th>Semester Offered</th>" +
             "<th>Class Code</th>" +
             "<th>Class Name</th>" +
-            "<th>Years offered</th>" +
             "<th>Already taken</th>"
         "</tr>";
 
@@ -59,13 +85,13 @@ function start(route, handle)
         {
             table += "<tr>";
             // iterate through for each column needed
-            for (let j = 0; j < tableArray[0].length; j++)
+            for (let j = 2; j < tableArray[0].length-1; j++)
             {
                 table += "<td>" + tableArray[i][j] + "</td>";
             }
             // the radio button for the final column, set the id to
             // equal the class code
-            table += "<td><input type='checkbox' id=" + tableArray[i][2] + " name=" + i + "></td>"
+            table += "<td><input type='checkbox' value='default' id=" + tableArray[i][2] + " name=" + i + "></td>"
             table += "</tr>";
 
         }
